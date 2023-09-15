3de71b03cdfb884888743fe72f3f927f93b7ba29
JoaoPedroFPedreira <joaopedro.ferreira.oruc@gmail.com>
2023-09-15 17:25:20 -0300

    Corrige diagrama uml do padrao

--- php/src/iIntermediadorPagamento.php
+++ php/src/iIntermediadorPagamento.php
@@ -4,7 +4,7 @@ namespace Factory\Php;
 
 interface iIntermediadorPagamento
 {
-    public function pix(String $dadosPedido, String $dadosUsuario);
-    public function boleto(String $dadosPedido, String $dadosUsuario);
-    public function credito(String $dadosPedido, String $dadosUsuario);
+    public function pix(String $dadosPedido, String $dadosUsuario): String;
+    public function boleto(String $dadosPedido, String $dadosUsuario): String;
+    public function credito(String $dadosPedido, String $dadosUsuario): String;
 }
