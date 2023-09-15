641836c6acd64340060a61a7188a1de044878122
JoaoPedroFPedreira <joaopedro.ferreira.oruc@gmail.com>
2023-09-15 09:52:35 -0300

    Finaliza testes para MercadoPago

--- src/test/java/MercadoPagoTest.java
+++ src/test/java/MercadoPagoTest.java
@@ -1,6 +1,6 @@
 import factory.MercadoPago;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,13 +10,24 @@ public class MercadoPagoTest {
 
     @BeforeEach
     public void setUp() {
-        mercadoPago = new MercadoPago();
-        System.out.println("dale");
+        this.mercadoPago = new MercadoPago();
     }
 
     @Test
     public void deveRealizarPedidoPixComSucesso() {
-        String response = mercadoPago.pix("dados do pedido", "dados do usuario");
+        String response = this.mercadoPago.pix("dados do pedido", "dados do usuario");
         assertEquals("dados do pagamento com pix no Mercado Pago", response);
+    }
+
+    @Test
+    public void deveRealizarPedidoBoletoComSucesso() {
+        String response = this.mercadoPago.boleto("dados do pedido", "dados do usuario");
+        assertEquals("dados do pagamento com boleto no Mercado Pago", response);
+    }
+
+    @Test
+    public void deveRealizarPedidoCreditoComSucesso() {
+        String response = this.mercadoPago.credito("dados do pedido", "dados do usuario");
+        assertEquals("dados do pagamento com credito no Mercado Pago", response);
     }
 }
